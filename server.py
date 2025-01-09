fab8389ec80eb31baf1d2fc085560e84970a3e68
Abhishek Kumar <98731119+thekrabhishek@users.noreply.github.com>
2025-01-10 00:49:08 +0530

    fg

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ def get_files():
     files = [
         {"name": "Chemistry Notes Chapter 1", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry"},
         {"name": "Chemistry Notes Chapter 2", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter2.pdf", "category": "Chemistry"},
-        {"name": "Physics Notes Chapter 1", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter1.pdf", "category": "Physics"},
-        {"name": "Physics Notes Chapter 2", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter2.pdf", "category": "Physics"},
+        {"name": "Physics Notes Chapter 1", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter1.pdf", "category": "Electrical_pyq"},
+        {"name": "Physics Notes Chapter 2", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter2.pdf", "category": "Electrical_notes"},
         {"name": "Chemistry Notes Chapter 3", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter3.pdf", "category": "Chemistry_pyq"},
         {"name": "Physics Notes Chapter 3", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter3.pdf", "category": "Physics"}
     ]
