257628c52860fabda686e781af3c218ba6bb675a
Abhishek Kumar <98731119+thekrabhishek@users.noreply.github.com>
2025-01-10 01:35:38 +0530

    update server

--- server.py
+++ server.py
@@ -2,10 +2,8 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-# Endpoint to serve files data
 @app.route('/files', methods=['GET'])
 def get_files():
-    # Example data with GitHub raw URLs
     files = [
         {"name": "Chemistry Notes Chapter 1", "url": "https://abhi-42i.pages.dev/B.Tech_2nd_Yr_CSE_v3.pdf", "category": "Chemistry"},
         {"name": "Chemistry Notes Chapter 2", "url": "https://raw.githubusercontent.com/username/repository/branch/path/to/chapter2.pdf", "category": "Chemistry"},
@@ -16,5 +14,12 @@ def get_files():
     ]
     return jsonify(files)
 
+
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port)
+
+
+
